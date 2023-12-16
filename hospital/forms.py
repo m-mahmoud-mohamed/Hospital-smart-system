82b0df458f342e4e7c7e9b28a0007150abcd1bf8
Mahmoud Mohamed <78882792+m-mahmoud-mohamed@users.noreply.github.com>
2023-12-16 16:07:57 +0200

    file uploaded

---
+++ hospital/forms.py
@@ -0,0 +1,62 @@
+from django import forms
+from django.contrib.auth.models import User
+from . import models
+
+
+
+class AdminSigupForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
+
+
+class DoctorUserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
+class DoctorForm(forms.ModelForm):
+    class Meta:
+        model=models.Doctor
+        fields=['address','mobile','department','status','profile_pic']
+
+
+
+class PatientUserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
+class PatientForm(forms.ModelForm):
+
+    assignedDoctorId=forms.ModelChoiceField(queryset=models.Doctor.objects.all().filter(status=True),empty_label="Name and Department", to_field_name="user_id")
+    class Meta:
+        model=models.Patient
+        fields=['address','mobile','status','symptoms','profile_pic']
+
+
+
+class AppointmentForm(forms.ModelForm):
+    doctorId=forms.ModelChoiceField(queryset=models.Doctor.objects.all().filter(status=True),empty_label="Doctor Name and Department", to_field_name="user_id")
+    patientId=forms.ModelChoiceField(queryset=models.Patient.objects.all().filter(status=True),empty_label="Patient Name and Symptoms", to_field_name="user_id")
+    class Meta:
+        model=models.Appointment
+        fields=['description','status']
+
+
+class PatientAppointmentForm(forms.ModelForm):
+    doctorId=forms.ModelChoiceField(queryset=models.Doctor.objects.all().filter(status=True),empty_label="Doctor Name and Department", to_field_name="user_id")
+    class Meta:
+        model=models.Appointment
+        fields=['description','status']
+
+
+
+
